1fd5faef25b8a8aa9f9be61d02b466d2b4d7d7fc
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-05 01:41:26 +0600

    routes added

--- about_us/urls.py
+++ about_us/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
-from restaurant import views
+from about_us import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.home),
-    path("meal/", include("meal.urls")),
-    path("about/", include("about_us.urls")),
+    path("", views.about),
 ]
